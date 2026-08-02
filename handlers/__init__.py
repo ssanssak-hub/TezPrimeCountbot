668f40a70459cc04d7c978326b20b04253658692
ssanssak-hub <ssanssak@gmail.com>
2026-08-02 11:53:11 +0330

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,4 @@
+# این فایل برای معرفی پکیج handlers استفاده می‌شود
+from .exam_handler import show_exam_details, handle_exam_menu, handle_exam_selection
+from .admin_handler import handle_admin_panel, handle_admin_commands, is_admin
+from .menu_handler import handle_menu_buttons
