1db9552aa52d9aef2b9a51ee26ffd928f724fabb
ssanssak-hub <ssanssak@gmail.com>
2026-08-05 13:20:13 +0330

    Update admin_handlers.py

--- admin/admin_handlers.py
+++ admin/admin_handlers.py
@@ -16,7 +16,9 @@ from admin.admin_keyboards import (
     admin_panel_keyboard, admin_manage_admins_keyboard,
     admin_manage_users_keyboard, admin_bot_status_keyboard,
     admin_broadcasts_list_keyboard, broadcast_action_keyboard,
-    back_to_admin_keyboard
+    back_to_admin_keyboard, permissions_selection_keyboard,   
+    admin_confirm_add_keyboard,
+    get_permission_name 
 )
 from admin.admin_database import (
     init_admin_db, save_broadcast, get_all_broadcasts,
