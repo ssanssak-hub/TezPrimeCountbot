914432b555b17fae6576c12d237ec4de36b260de
ssanssak-hub <ssanssak@gmail.com>
2026-08-07 14:14:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -673,7 +673,11 @@ def setup_handlers():
             ],            
             BROADCAST_TITLE: [
                 MessageHandler(filters.TEXT & ~filters.COMMAND, broadcast_now_message),
-                MessageHandler(filters.PHOTO | filters.VIDEO | filters.Document.AUDIO | filters.Document.VIDEO | filters.Document.ALL, handle_file_receive),
+                MessageHandler(filters.PHOTO, handle_file_receive),
+                MessageHandler(filters.VIDEO, handle_file_receive),
+                MessageHandler(filters.Document.ALL, handle_file_receive),
+                MessageHandler(filters.AUDIO, handle_file_receive),
+                MessageHandler(filters.VOICE, handle_file_receive),
             ],
             BROADCAST_MESSAGE: [
                 MessageHandler(filters.TEXT & ~filters.COMMAND, broadcast_now_message),
