1c77f2fc179274262be091da496edf7775e43f71
ssanssak-hub <ssanssak@gmail.com>
2026-08-07 20:47:45 +0330

    Update admin_handlers.py

--- admin/admin_handlers.py
+++ admin/admin_handlers.py
@@ -901,7 +901,7 @@ async def handle_file_receive(update: Update, context: ContextTypes.DEFAULT_TYPE
     # ✅ چک کن فوروارد شده یا نه (همه روش‌ها)
     if message.forward_origin:
         is_forward = True
-        from_message_id = message.message_id
+        from_message_id = origin.message_id
         origin = message.forward_origin
         if hasattr(origin, 'chat') and origin.chat:
             from_chat_id = str(origin.chat.id)
